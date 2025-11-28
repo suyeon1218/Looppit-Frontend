c038e60533bcd6f433f5927880ae14cac921ccd5
yerin <dev.yerin@gmail.com>
2025-11-29 00:17:35 +0900

    feat(route): 라우트 가드 미들웨어 구현

---
+++ proxy.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from "next/server";
+import { guardRules } from "@/shared/proxy/proxy.guard";
+
+const proxy = (request: NextRequest) => {
+  const matchedRule = guardRules.find((rule) => rule.when(request));
+
+  if (matchedRule) {
+    return NextResponse.redirect(new URL(matchedRule.redirect, request.url));
+  }
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+};
+
+export default proxy;
