668d28e3c6dd4b99088fc860b25b7416e282a6ea
Suyeon <msjgr2801@gmail.com>
2025-12-02 15:14:24 +0900

    feat(login): 에러 관리 메세지 추가
    - zod에서 확장된 타입을 기반으로 상수를 작성할 수 있도록 함
    - loginSchema를 사용하여 키 타입을 만들경우 순환참조가 발생하므로 타입과 스키마를 분리

--- domains/login/login.types.ts
+++ domains/login/login.types.ts
@@ -1,8 +1,23 @@
 import { z } from 'zod';
 
+import { ExtendedZodErrorMessageMap } from '@/shared/types';
+
+import { LOGIN_ERROR_MESSAGES } from './login.constants';
+
+export type LoginErrorMessageKeys = 'email' | 'password';
+
 export const loginFormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z
+    .string(LOGIN_ERROR_MESSAGES.email.invalid_type)
+    .min(1, LOGIN_ERROR_MESSAGES.email.too_small)
+    .email(LOGIN_ERROR_MESSAGES.email.invalid_type),
+  password: z
+    .string(LOGIN_ERROR_MESSAGES.password.invalid_type)
+    .min(1, LOGIN_ERROR_MESSAGES.password.too_small),
 });
 
 export type LoginFormValues = z.infer<typeof loginFormSchema>;
+export type LoginErrorMessageValues = Record<
+  LoginErrorMessageKeys,
+  Partial<ExtendedZodErrorMessageMap>
+>;
