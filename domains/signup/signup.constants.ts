5e497a6b1166e72c8e1c5d4ee10b3be62650027f
Suyeon <msjgr2801@gmail.com>
2025-12-02 16:17:30 +0900

    feat(signup): 에러메세지 문구 수정

--- domains/signup/signup.constants.ts
+++ domains/signup/signup.constants.ts
@@ -9,11 +9,11 @@ type SignupErrorMessageValues = Record<
 
 export const SIGNUP_ERROR_MESSAGES: SignupErrorMessageValues = {
   email: {
-    invalid_type: '이메일 형식이 올바르지 않습니다.',
+    invalid_type: '이메일 형식이 올바르지 않아요.',
     too_small: '이메일을 입력해주세요.',
   },
   password: {
-    invalid_type: '올바른 비밀번호를 입력해주세요.',
+    invalid_type: '비밀번호가 안전하지 않아요.',
     too_small: '비밀번호는 최소 8자 이상 입력해주세요.',
     too_big: '비밀번호는 최대 20자 이하로 입력해주세요.',
   },
