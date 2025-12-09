83750feb2ad979a88269bdadb1d2bbc40dd7d581
Suyeon <msjgr2801@gmail.com>
2025-12-09 20:37:41 +0900

    refactor(shared): 타임 포매팅 함수의 인자 명시적으로 작성

--- shared/utils/time-format.ts
+++ shared/utils/time-format.ts
@@ -1,9 +1,9 @@
-export function formatTime(time: number, format: 'mm:ss' | 'ss') {
+export function formatTime(seconds: number, format: 'mm:ss' | 'ss') {
   if (format === 'mm:ss') {
-    return `${Math.floor(time / 60)
+    return `${Math.floor(seconds / 60)
       .toString()
-      .padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
+      .padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`;
   }
 
-  return `${time}`;
+  return `${seconds}`;
 }
