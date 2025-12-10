9c8eadb32b9dde1418a18f127a3922b34bcd0dbe
Suyeon <msjgr2801@gmail.com>
2025-12-10 20:27:15 +0900

    feat(shared): 리프레시 토큰용 아톰 선언

--- shared/store/auth.atom.ts
+++ shared/store/auth.atom.ts
@@ -1,7 +1,13 @@
-import { atom, useAtomValue, useSetAtom } from "jotai";
+import { atom, useAtomValue, useSetAtom } from 'jotai';
 
 export const tokenAtom = atom<string | null>(null);
 
 export const useTokenAtomValue = () => useAtomValue(tokenAtom);
 
 export const useSetTokenAtom = () => useSetAtom(tokenAtom);
+
+export const refreshTokenAtom = atom<string | null>(null);
+
+export const useRefreshTokenAtomValue = () => useAtomValue(refreshTokenAtom);
+
+export const useSetRefreshTokenAtom = () => useSetAtom(refreshTokenAtom);
