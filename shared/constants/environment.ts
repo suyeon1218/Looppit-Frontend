48b84d860b800585ebdc3de016294033de9ebfee
Suyeon <msjgr2801@gmail.com>
2025-12-11 22:04:34 +0900

    feat(shared): 환경변수를 상수로 만들어 관리하기

--- shared/constants/environment.ts
+++ shared/constants/environment.ts
@@ -1,1 +1,11 @@
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+export const envs = {
+  urls: {
+    apiEndPoint: process.env.NEXT_PUBLIC_API_BASE_URL!,
+  },
+} as const;
+
+export const PROJECT_ENV = {
+  ...envs.urls,
+} as const;
---
+++ shared/utils/env.ts
@@ -0,0 +1,21 @@
+import { PROJECT_ENV } from '@/shared/constants';
+
+const validateEnv = (): typeof PROJECT_ENV => {
+  const missingKey = Object.entries(PROJECT_ENV).some(([, value]) => !value);
+
+  if (missingKey) {
+    throw new Error(`환경 변수 ${missingKey}가 설정되지 않았습니다.`);
+  }
+
+  return PROJECT_ENV;
+};
+
+const VALIDATED_PROJECT_ENV = validateEnv();
+
+/**
+ * 프로젝트 설정 정보를 반환하는 유틸리티 함수
+ * @returns 프로젝트 설정 객체
+ */
+export const getProjectConfig = () => {
+  return VALIDATED_PROJECT_ENV;
+};
