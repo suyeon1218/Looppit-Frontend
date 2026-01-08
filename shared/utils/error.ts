4322da1df9962e3468e3f986dd6404aaf0b3a14c
Suyeon <msjgr2801@gmail.com>
2026-01-08 17:46:58 +0900

    feat(error): 에러 전용 Next Response 생성 함수 구현 - error.ts: 에러와 기본 메세지를 받아 NextResponse 로 반환

--- shared/utils/error.ts
+++ shared/utils/error.ts
@@ -1,3 +1,7 @@
+import { NextResponse } from 'next/server';
+
+import { AxiosError } from 'axios';
+
 /**
  * Error 객체인지 확인하는 타입 가드
  * @param error - 확인할 에러 객체
@@ -18,4 +22,17 @@ export const getErrorMessage = (
   defaultMessage: string,
 ): string => {
   return isError(error) ? error.message : defaultMessage;
+};
+
+export const makeNextResponseError = (
+  error: unknown,
+  defaultMessage: string = '알 수 없는 에러가 발생했습니다.',
+) => {
+  const errorCode = error instanceof AxiosError ? error.response?.status : 500;
+  const errorMessage =
+    error instanceof AxiosError
+      ? error.response?.data?.message
+      : defaultMessage;
+
+  return NextResponse.json({ message: errorMessage }, { status: errorCode });
 };
