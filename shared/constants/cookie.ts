c20c68937153aea49721004ea89bbb9b0fdb234b
Suyeon <msjgr2801@gmail.com>
2026-01-10 17:16:56 +0900

    feat(token): 불필요한 쿠키 관련 로직 삭제 - cookie.ts: 사용하지 않는 상수 제거 - token.ts: 백엔드에서 옵션을 설정해주므로 제거

--- shared/constants/cookie.ts
+++ shared/constants/cookie.ts
@@ -2,8 +2,3 @@ export const COOKIE_KEYS = {
   ACCESS_TOKEN: 'ACCESS_TOKEN',
   REFRESH_TOKEN: 'REFRESH_TOKEN',
 } as const;
-
-export const COOKIE_OPTIONS = {
-  MAX_AGE: 60 * 5,
-  MAX_AGE_REFRESH: 60 * 60 * 24 * 7,
-} as const;
--- shared/utils/token.ts
+++ shared/utils/token.ts
@@ -1,28 +1,6 @@
-import { deleteCookie, setCookie } from '@/shared/utils';
+import { deleteCookie } from '@/shared/utils';
 
-import { COOKIE_KEYS, COOKIE_OPTIONS } from '../constants';
-
-export const setAccessTokenToCookie = async (accessToken: string) => {
-  await setCookie(COOKIE_KEYS.ACCESS_TOKEN, accessToken, {
-    maxAge: COOKIE_OPTIONS.MAX_AGE,
-  });
-};
-
-export const setRefreshTokenToCookie = async (refreshToken: string) => {
-  await setCookie(COOKIE_KEYS.REFRESH_TOKEN, refreshToken, {
-    maxAge: COOKIE_OPTIONS.MAX_AGE_REFRESH,
-  });
-};
-
-type Tokens = {
-  accessToken: string;
-  refreshToken: string;
-};
-
-export const setTokensToCookies = async (data: Tokens) => {
-  await setAccessTokenToCookie(data.accessToken);
-  await setRefreshTokenToCookie(data.refreshToken);
-};
+import { COOKIE_KEYS } from '../constants';
 
 export const removeTokensFromCookies = async () => {
   await deleteCookie(COOKIE_KEYS.ACCESS_TOKEN);
