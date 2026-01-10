80460b4215558e43434f16acfdf11a30ba7392af
Suyeon <msjgr2801@gmail.com>
2026-01-10 17:12:04 +0900

    feat(login): api route를 server action으로 변경 - route.ts: api route 삭제 - login.ts: login api route의 로직을 postLogin으로 이동 - api.next-server-client.ts: api route 전용 axios client 삭제 - environment: api route용 url 프로퍼티 삭제

--- domains/login/api/login.ts
+++ domains/login/api/login.ts
@@ -1,7 +1,45 @@
-import { apiNextServerClient } from '@/shared/api/api.next-server-client';
+'use server';
+
+import { NextResponse } from 'next/server';
+
+import { AxiosError } from 'axios';
+
+import { apiServerClient } from '@/shared/api/api.server-client';
+import { applySetCookieHeader } from '@/shared/utils';
 
 export const postLogin = async (formData: FormData) => {
-  return await apiNextServerClient.post('/auth/login', formData, {
-    'Content-Type': 'multipart/form-data',
-  });
+  try {
+    const response = await apiServerClient.requestRaw('/user/login', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    const setCookieHeaders = response.headers['set-cookie'];
+
+    if (setCookieHeaders && Array.isArray(setCookieHeaders)) {
+      await applySetCookieHeader(setCookieHeaders);
+    }
+
+    return {
+      code: 'HTTP_200',
+      message: '로그인에 성공했습니다.',
+    };
+  } catch (error) {
+    const errorCode =
+      error instanceof AxiosError ? error.response?.status : 500;
+    const errorMessage =
+      error instanceof AxiosError
+        ? error.response?.data?.message
+        : '로그인에 실패했습니다. 잠시 후 다시 시도해주세요';
+
+    return NextResponse.json(
+      {
+        message: errorMessage,
+      },
+      { status: errorCode },
+    );
+  }
 };
--- shared/constants/environment.ts
+++ shared/constants/environment.ts
@@ -6,7 +6,6 @@ const envs = {
     isProduction: process.env.NODE_ENV === 'production',
   },
   urls: {
-    nextServerApiEndPoint: '/api',
     apiEndPoint: process.env.NEXT_PUBLIC_API_BASE_URL!,
   },
 } as const;
