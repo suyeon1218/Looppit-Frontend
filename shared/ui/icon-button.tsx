3d09b9040601f356fc5d0a5a37abe486fb0fa8b6
yerin <dev.yerin@gmail.com>
2026-01-14 01:08:01 +0900

    feat(iconButton): iconButton 추가

---
+++ shared/ui/icon-button.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import { Slot } from '@radix-ui/react-slot';
+
+import { Icon, IconName, IconSize } from '@/shared/ui/icon';
+import { cn } from '@/shared/utils';
+
+export type IconButtonSize = '28' | '36' | '40';
+
+const iconButtonVariants: Record<
+  IconButtonSize,
+  { iconSize: IconSize; className: string }
+> = {
+  '28': {
+    iconSize: '24',
+    className: 'size-[28px]',
+  },
+  '36': {
+    iconSize: '18',
+    className: 'size-[36px]',
+  },
+  '40': {
+    iconSize: '24',
+    className: 'size-[40px]',
+  },
+};
+
+type ButtonProps = React.ComponentProps<'button'> & {
+  asChild?: boolean;
+  icon: IconName;
+  iconClassName?: string;
+  size: IconButtonSize;
+};
+
+const IconButton = ({
+  className,
+  icon,
+  iconClassName,
+  size = '28',
+  asChild = false,
+  type = 'button',
+  ...props
+}: ButtonProps) => {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      type={type}
+      className={cn(
+        'rounded-full transition-all active:scale-90 border shadow-sm bg-card border-white-soft',
+        iconButtonVariants[size].className,
+        className,
+      )}
+      {...props}
+    >
+      <Icon
+        icon={icon}
+        className={iconClassName}
+        size={iconButtonVariants[size].iconSize}
+      />
+    </Comp>
+  );
+};
+
+export { IconButton };
