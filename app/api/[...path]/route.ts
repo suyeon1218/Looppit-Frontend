e2e97fb23c5ffe8556cae426b47c76083c7389a5
Suyeon <msjgr2801@gmail.com>
2026-01-14 21:04:38 +0900

    refactor(api-route): 통합된 요청 처리 로직 추가 - route.ts: GET, POST, PUT, DELETE 메서드의 요청 처리 로직을 handleRequest 함수로 통합하여 코드 중복 제거 및 body 처리 로직 추가

--- app/api/[...path]/route.ts
+++ app/api/[...path]/route.ts
@@ -1,3 +1,4 @@
+import { HTTP_METHOD } from 'next/dist/server/web/http';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -20,61 +21,67 @@ const makeHeadersWithCookie = async (request: NextRequest) => {
   };
 };
 
-export async function GET(request: NextRequest) {
-  const headers = await makeHeadersWithCookie(request);
-  const endpoint = getEndpoint(request.nextUrl);
+const getRequestBody = async (request: NextRequest) => {
+  const contentType = request.headers.get('content-type');
 
+  if (!contentType) return undefined;
+
+  if (contentType.includes('application/json')) {
+    return await request.json();
+  }
+
+  if (contentType.includes('multipart/form-data')) {
+    return await request.formData();
+  }
+
+  return await request.text();
+};
+
+const handleRequest = async (
+  request: NextRequest,
+  method: HTTP_METHOD,
+): Promise<NextResponse> => {
   try {
-    const response = await apiServerClient.get(endpoint, headers);
+    const headers = await makeHeadersWithCookie(request);
+    const endpoint = getEndpoint(request.nextUrl);
+    const body = await getRequestBody(request);
+
+    let response;
+
+    switch (method) {
+      case 'GET':
+        response = await apiServerClient.get(endpoint, headers);
+        break;
+      case 'POST':
+        response = await apiServerClient.post(endpoint, body, headers);
+        break;
+      case 'PUT':
+        response = await apiServerClient.put(endpoint, body, headers);
+        break;
+      case 'DELETE':
+        response = await apiServerClient.delete(endpoint, headers);
+        break;
+    }
+
     return NextResponse.json(response);
   } catch (error) {
     const apiError = createApiError(error);
     return makeNextResponseError(apiError);
   }
+};
+
+export async function GET(request: NextRequest) {
+  return await handleRequest(request, 'GET');
 }
 
 export async function POST(request: NextRequest) {
-  const headers = await makeHeadersWithCookie(request);
-  const endpoint = getEndpoint(request.nextUrl);
-
-  try {
-    const response = await apiServerClient.post(
-      endpoint,
-      request.body,
-      headers,
-    );
-
-    return NextResponse.json(response);
-  } catch (error) {
-    const apiError = createApiError(error);
-    return makeNextResponseError(apiError);
-  }
+  return await handleRequest(request, 'POST');
 }
 
 export async function PUT(request: NextRequest) {
-  const headers = await makeHeadersWithCookie(request);
-  const endpoint = getEndpoint(request.nextUrl);
-
-  try {
-    const response = await apiServerClient.put(endpoint, request.body, headers);
-
-    return NextResponse.json(response);
-  } catch (error) {
-    const apiError = createApiError(error);
-    return makeNextResponseError(apiError);
-  }
+  return await handleRequest(request, 'PUT');
 }
 
 export async function DELETE(request: NextRequest) {
-  const headers = await makeHeadersWithCookie(request);
-  const endpoint = getEndpoint(request.nextUrl);
-
-  try {
-    const response = await apiServerClient.delete(endpoint, headers);
-
-    return NextResponse.json(response);
-  } catch (error) {
-    const apiError = createApiError(error);
-    return makeNextResponseError(apiError);
-  }
+  return await handleRequest(request, 'DELETE');
 }
