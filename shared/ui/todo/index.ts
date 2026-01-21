c5406372393c1262c1559e92f4da640804524d16
yerin <dev.yerin@gmail.com>
2026-01-22 03:54:08 +0900

    refactor(shared/todo): index 중복 제거

--- shared/ui/todo/index.ts
+++ shared/ui/todo/index.ts
@@ -1,5 +1,2 @@
-export * from '../../../domains/home/ui/sheets/category-select-sheet';
-export * from '../../../domains/home/ui/sheets/todo-form-sheet';
-export * from '../../../domains/home/ui/sheets/todo-form-sheet.ui';
 export * from './todo-item';
 export * from './todo-item-header';
