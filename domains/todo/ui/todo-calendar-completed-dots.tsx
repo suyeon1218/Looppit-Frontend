8d8dee5400e46a28950dfb82395d902b908e7fc0
Suyeon <msjgr2801@gmail.com>
2026-01-25 16:01:25 +0900

    fix(todo-calendar): 프로퍼티 타입 변경에 따른 수정 - todo-calendar.tsx: day 프로퍼티 함께 전달 - todo-calendar-completed-dots.tsx: 카테고리 데이터 타입 변경에 따른 수정

--- domains/todo/ui/todo-calendar-completed-dots.tsx
+++ domains/todo/ui/todo-calendar-completed-dots.tsx
@@ -1,19 +1,33 @@
+import { DayProps } from 'react-day-picker';
+
+import { CompletedCategoryData } from '@/domains/home/utils';
+
 interface TodoCalendarCompletedDotsProps {
-  colors: { color: string; id: string }[];
+  completedColorsByDate: CompletedCategoryData;
+  day: DayProps['day'];
 }
 
 export function TodoCalendarCompletedDots({
-  colors,
+  day,
+  completedColorsByDate,
 }: TodoCalendarCompletedDotsProps) {
+  const targetDate = day.date.toISOString().split('T')[0];
+  const targetCompletedColors = completedColorsByDate[targetDate];
+
+  if (!targetCompletedColors) return null;
+
   return (
-    <div className="h-1 max-w-6 flex flex-wrap items-center justify-center gap-[2px]">
-      {colors.map((color) => (
-        <div
-          key={color.id}
-          className="w-[3px] h-[3px] rounded-full"
-          style={{ backgroundColor: color.color }}
-        ></div>
-      ))}
+    <div className="rdp-sub-day-dot-wrapper h-1 max-w-6 flex flex-wrap items-center justify-center gap-[2px]">
+      {Object.entries(targetCompletedColors).map(
+        ([key, value]) =>
+          value.completed && (
+            <div
+              key={key}
+              className="w-[3px] h-[3px] rounded-full"
+              style={{ backgroundColor: value.color }}
+            ></div>
+          ),
+      )}
     </div>
   );
 }
